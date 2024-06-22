1dfe8fd4134a536bc6eeaa7a8ab25106c7ec9c4c
suriya-wsl <suriyaprakhash@gmail.com>
2024-06-22 14:31:35 -0400

    updated deploy command

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  // dev: false,
+  // distDir: 'dist',
+  output: 'export',
+  images: { unoptimized: true, },
+  // poweredByHeader: false,
+  // Use the CDN in production and localhost for development - https://www.jsdelivr.com/github
+  assetPrefix: isProd ? 'https://cdn.jsdelivr.net/gh/suriyaprakhash/pixie-generator@gh-pages' : undefined,
+}
+
+
+
+  export default nextConfig;
