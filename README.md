de706b3c5c76ef77241146a429a37badc4d0b286
suriyaprakhash <suriyaprakhash@gmail.com>
2024-06-28 23:02:51 -0400

    added analytics and updated readme

--- README.md
+++ README.md
@@ -7,8 +7,17 @@ Running in local
 npm run dev
 ```
 
+Building for Prod
+```
+npm run build
+```
+
+## Features
+
+- The application can generate random PKCE code verifier and its corresponding challenge.
+- It can generate them in bulk CSV which can be later used for automation testing.
+- It can be publicly accessed at http://api.pixie.suriyaprakhash.com, which returns the verfier and challenge which can then used on demand 
+
 ## URL
 
 The application can be found live at https://pixie.suriyaprakhash.com
-
-The application can generate bulk 1000 record at https://pixie.suriyaprakhash.com/bulk and it can do one time html document with a JSON type on https://pixie.suriyaprakhash.com/json
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div>
+          <Script id="ga4-script1"
+            strategy="afterInteractive" // Recommended for GTM
+            src="https://www.googletagmanager.com/gtag/js?id=G-G5ND1DJTRH"
+          />
+          <Script id="ga-script2"
+            strategy="afterInteractive" // Recommended for GTM
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G5ND1DJTRH');
+          `,
+            }}
+          />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
